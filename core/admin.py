4efbd88adde12f86b0b917a1310582b376691844
Parbhat Puri <parbhatpuri17@gmail.com>
2016-03-02 17:51:26 +0530

    Employee model register in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from core.models import Employee
+
 # Register your models here.
+
+admin.site.register(Employee)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django_pandas.managers import DataFrameManager
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
+@python_2_unicode_compatible
 class Employee(models.Model):
 
     full_name = models.CharField(max_length=25)
@@ -11,3 +13,6 @@ class Employee(models.Model):
     wage = models.FloatField()
 
     objects = DataFrameManager()
+
+    def __str__(self):
+        return self.full_name
