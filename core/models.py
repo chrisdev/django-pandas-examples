9ac8672e865260d3524ef5717b4c2d2216d346d6
Parbhat Puri <parbhatpuri17@gmail.com>
2016-03-02 17:38:43 +0530

    read_frame and to_dataframe notebooks

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_pandas.managers import DataFrameManager
 
 # Create your models here.
+
+class Employee(models.Model):
+
+    full_name = models.CharField(max_length=25)
+    age = models.IntegerField()
+    department = models.CharField(max_length=3)
+    wage = models.FloatField()
+
+    objects = DataFrameManager()
